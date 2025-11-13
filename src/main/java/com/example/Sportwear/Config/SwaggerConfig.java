f740137ce73de0e2e44ebadab6fdcfc6f43dd80e
carlos <ca.calvio@duocuc.cl>
2025-11-13 13:18:41 -0300

    creacion de swagger configuration

---
+++ src/main/java/com/example/Sportwear/Config/SwaggerConfig.java
@@ -0,0 +1,20 @@
+package com.example.Sportwear.Config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Sporwear API")
+                        .version("1.1")
+                        .description("prueba para Ecomarket SPAweb para la gestion de usuario, productos y envios"));
+
+    }
+}
---
+++ src/main/java/com/example/Sportwear/Controller/UserController.java
@@ -0,0 +1,4 @@
+package com.example.Sportwear.Controller;
+
+public class UserController {
+}
