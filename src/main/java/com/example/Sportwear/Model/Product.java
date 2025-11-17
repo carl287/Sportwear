130a23ff6fdb0d9cc0eeae8cf65e104678bbd136
paula220004 <millaraymila75@gmail.com>
2025-11-17 14:43:11 -0300

    Se agregaron los servicios

--- src/main/java/com/example/Sportwear/Model/Product.java
+++ src/main/java/com/example/Sportwear/Model/Product.java
@@ -1,27 +1,19 @@
 package com.example.Sportwear.Model;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
+import lombok.EqualsAndHashCode;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-
+@EqualsAndHashCode(callSuper = true)
 @Entity
 public class Product extends GestionProduct {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
 
-    private int id;
     private String name;
     private String description;
     private int price;
-    private int stock;
-
 }
