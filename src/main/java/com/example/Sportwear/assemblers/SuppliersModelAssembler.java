32a612e7a2110f580c88c71d01a198fad1dc456f
carlos <ca.calvio@duocuc.cl>
2025-11-21 19:40:45 -0300

    corregi el supplier assembler

--- src/main/java/com/example/Sportwear/assemblers/SuppliersModelAssembler.java
+++ src/main/java/com/example/Sportwear/assemblers/SuppliersModelAssembler.java
@@ -1,14 +1,10 @@
 package com.example.Sportwear.assemblers;
 
-
 import com.example.Sportwear.Controller.SupplierController;
 import com.example.Sportwear.Model.Suppliers;
-
 import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.server.RepresentationModelAssembler;
 import org.springframework.stereotype.Component;
-
-// Se elimina la importación incorrecta: import java.util.function.Supplier;
 
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
@@ -18,11 +14,14 @@ public class SuppliersModelAssembler implements RepresentationModelAssembler<Sup
 
     @Override
     public EntityModel<Suppliers> toModel(Suppliers supplier) {
+        Suppliers placeholder = new Suppliers();
 
         return EntityModel.of(supplier,
                 linkTo(methodOn(SupplierController.class).getById(supplier.getId())).withSelfRel(),
                 linkTo(methodOn(SupplierController.class).getAll()).withRel("suppliers"),
-                linkTo(methodOn(SupplierController.class).delete(supplier.getId()))).withRel("delete-supplier")
+                linkTo(methodOn(SupplierController.class).create(placeholder.getId())).withRel("create"),
+                linkTo(methodOn(SupplierController.class).update(supplier.getId(), placeholder)).withRel("update"),
+                linkTo(methodOn(SupplierController.class).delete(supplier.getId())).withRel("delete")
         );
     }
 }
