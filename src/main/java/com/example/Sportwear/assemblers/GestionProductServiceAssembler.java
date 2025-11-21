d55cc8425a8b40f0834a1ba8029282b7a462bdc7
carlos <ca.calvio@duocuc.cl>
2025-11-21 15:09:24 -0300

    creacion de product service assembler

---
+++ src/main/java/com/example/Sportwear/assemblers/GestionProductServiceAssembler.java
@@ -0,0 +1,22 @@
+package com.example.Sportwear.assemblers;
+
+import com.example.Sportwear.Controller.GestionProductController;
+import com.example.Sportwear.Model.GestionProduct; // Asumido: El modelo de datos
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.RepresentationModelAssembler;
+import org.springframework.stereotype.Component;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+@Component
+public class GestionProductServiceAssembler implements RepresentationModelAssembler<GestionProduct, EntityModel<GestionProduct>> {
+
+    @Override
+    public EntityModel<GestionProduct> toModel(GestionProduct gestionProduct) {
+        return EntityModel.of(gestionProduct,
+                linkTo(methodOn(GestionProductController.class).increaseStock(gestionProduct.getId(), 0)).withRel("aumentar-stock"),
+                linkTo(methodOn(GestionProductController.class).decreaseStock(gestionProduct.getId(), 0)).withRel("disminuir-stock")
+        );
+    }
+}
