8d62cd6588a7eaf6505b61359fbfdda954e9a2e0
carl287 <ca.calvio@duocuc.cl>
2025-11-30 16:45:21 -0300

    Correccion de tabla repetida

--- src/main/java/com/example/Sportwear/Model/Product.java
+++ src/main/java/com/example/Sportwear/Model/Product.java
@@ -1,12 +1,6 @@
 package com.example.Sportwear.Model;
 
-import jakarta.persistence.CollectionTable;
-import jakarta.persistence.Column;
-import jakarta.persistence.ElementCollection;
-import jakarta.persistence.Entity;
-import jakarta.persistence.MapKeyColumn;
-import jakarta.persistence.Table;
-import jakarta.persistence.JoinColumn;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -30,7 +24,7 @@ public class Product extends GestionProduct {
     private String description;
 
     @Column(name = "PRODUCT_PRICE")
-    private double price;
+    private int price;
 
     @Column(name = "PRODUCT_CATEGORY")
     private String category;
@@ -46,27 +40,10 @@ public class Product extends GestionProduct {
 
     @ElementCollection
     @CollectionTable(
-            name = "PRODUCT_STOCK_BY_SIZE",
+            name = "PRODUCT_STOCK_BY_SIZE", 
             joinColumns = @JoinColumn(name = "PRODUCT_ID")
     )
     @MapKeyColumn(name = "SIZE_KEY")
     @Column(name = "STOCK_QUANTITY")
     private Map<String, Integer> stockPorTalla = new HashMap<>();
-
-
-    public void decreaseStock(String sizeKey, int quantity) {
-        if (this.stockPorTalla.containsKey(sizeKey)) {
-            int currentStock = this.stockPorTalla.get(sizeKey);
-            if (currentStock >= quantity) {
-                this.stockPorTalla.put(sizeKey, currentStock - quantity);
-            } else {
-                throw new IllegalStateException("Stock insuficiente para la talla: " + sizeKey);
-            }
-        } else {
-            throw new IllegalArgumentException("Talla no válida: " + sizeKey);
-        }
-    }
 }
-
-
-
