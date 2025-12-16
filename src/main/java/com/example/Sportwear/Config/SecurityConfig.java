84491f8f626c2cee574bca501cb1497b8ee9a9ce
carl287 <ca.calvio@duocuc.cl>
2025-12-16 17:09:02 -0300

    arreglando security config

--- src/main/java/com/example/Sportwear/Config/SecurityConfig.java
+++ src/main/java/com/example/Sportwear/Config/SecurityConfig.java
@@ -18,7 +18,7 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/**").permitAll()
-                        .requestMatchers("user/**").permitAll()
+                        .requestMatchers("/users/**").permitAll()
                         .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
                         .anyRequest().permitAll()
                 )
